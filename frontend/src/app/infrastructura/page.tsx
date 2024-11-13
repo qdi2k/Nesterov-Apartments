109eb8ea8d69303786ef70b0d6aa5131a56b39e8
Danil Novgorodov <1ceteam2403@gmail.com>
2024-11-13 15:00:59 +0300

    инициализация проекта

---
+++ frontend/src/app/infrastructura/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Infrastructura() {
+    return (
+      <div>
+        Infrastructura page
+      </div>
+    );
+  }
+  
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+import HomePage from "@/pages/main/home";
+
+export default function Home() {
+  return (
+    <HomePage />
+  );
+}
