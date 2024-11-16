4c57881721461e2576fb400fd5e7e0887c2970ad
Danil Novgorodov <1ceteam2403@gmail.com>
2024-11-16 16:12:20 +0300

    Верстка футера

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type {Metadata} from "next"
+import "./globals.css"
+import Footer from "@/widgets/footer/Footer"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,8 @@ export default function RootLayout(
     <html lang="en">
       <body>
         {children}
+        <Footer/>
       </body>
     </html>
-  );
+  )
 }
