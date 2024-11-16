54333d33b150ab064565952490ce05461ab561a8
Danil Novgorodov <1ceteam2403@gmail.com>
2024-11-16 16:11:55 +0300

    Верстка Ui-компоментов ()

---
+++ frontend/src/app/about-builder/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutBuilder() {
+  return (
+    <div>
+      About page
+    </div>
+  );
+}
--- frontend/src/app/about-project/page.tsx
+++ frontend/src/app/about-project/page.tsx
@@ -1,5 +1,5 @@
 
-export default function About() {
+export default function AboutProject() {
     return (
       <div>
         About page
