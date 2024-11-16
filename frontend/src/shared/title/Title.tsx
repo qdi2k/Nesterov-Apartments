0266db80161e612eecd6b0d97c1bb13ef7821f15
Danil Novgorodov <1ceteam2403@gmail.com>
2024-11-16 16:09:43 +0300

    Обновил стили

--- frontend/src/shared/title/Title.tsx
+++ frontend/src/shared/title/Title.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from 'react'
 import localFont from "next/font/local"
 import themeStyles from '@/shared/theme.module.css'
 
-const geistMono = localFont({
+const helio = localFont({
   src: "./../heliosextthin.otf",
   variable: "--font-geist-mono",
   weight: "100 200 300 400 500 600 700 800 900",
@@ -14,7 +14,7 @@ interface ITitleProps {
 
 export default function Title({children}: ITitleProps) {
   return (
-    <h2 className={`${geistMono.className} ${themeStyles.large} ${themeStyles.regular} ${themeStyles.white}`}>
+    <h2 className={`${helio.className} ${themeStyles.large} ${themeStyles.regular} ${themeStyles.white}`}>
       {children}
     </h2>
   );
