4dfe13765cc485fa9d352cf4c6f4c058ed710961
qdi2k <qdi2k@mail.ru>
2025-03-02 01:24:13 +0300

    Добавил новое поле в apartments rooms_count

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,21 +1,27 @@
-from sqlalchemy import Integer, String, Float
+from sqlalchemy import Integer, String, Float, Enum
 from sqlalchemy.orm import mapped_column, Mapped
 from fastapi_storages.integrations.sqlalchemy import FileType
 
 from app.db.database import Base
 from app.core.config import APARTMENTS_IMAGE
+from core.enums import CountRooms
 
 
 class Apartment(Base):
     __tablename__ = 'apartments'
 
     name: Mapped[str] = mapped_column(String, nullable=False)
-    project: Mapped[str] = mapped_column(String, nullable=False)
 
+    project: Mapped[str] = mapped_column(String, nullable=False)
     address: Mapped[str] = mapped_column(String, nullable=False)
+
+    rooms_count: Mapped[CountRooms] = mapped_column(
+        Enum(CountRooms), nullable=False
+    )
     section: Mapped[str] = mapped_column(String, nullable=False)
     floor: Mapped[int] = mapped_column(Integer, nullable=False)
     area: Mapped[float] = mapped_column(Float, nullable=False)
+
     image: Mapped[str] = mapped_column(
         FileType(storage=APARTMENTS_IMAGE), nullable=True
     )
