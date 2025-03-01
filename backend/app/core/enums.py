c3bf38da3bab4eb85057764b47223857ffbd1ae3
qdi2k <qdi2k@mail.ru>
2025-03-02 01:24:13 +0300

    Добавил новое поле в apartments rooms_count

--- backend/app/core/enums.py
+++ backend/app/core/enums.py
@@ -4,3 +4,12 @@ from enum import Enum
 class CallStatusEnum(Enum):
     AWAIT = "await"
     COMPLETED = "completed"
+
+
+class CountRooms(Enum):
+    STUDIO = "studio"
+    ONE = "1"
+    TWO = "2"
+    THREE = "3"
+    FOUR = "4"
+    FIVE = "5"
