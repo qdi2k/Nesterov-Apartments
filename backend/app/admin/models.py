b9181f0c06f3a9a411d41724a935931f703b27bf
qdi2k <qdi2k@mail.ru>
2025-03-02 02:35:15 +0300

    Новая модель Zone для админки

--- backend/app/admin/models.py
+++ backend/app/admin/models.py
@@ -1,13 +1,21 @@
 from sqladmin import ModelView
 
-from app.db.models import Apartment
+from app.db.models import Apartment, Zone
+
+
+class ZoneAdmin(ModelView, model=Zone):
+    column_list = [Zone.id, Zone.city, Zone.district]
+    column_searchable_list = [Zone.city, Zone.district]
+    column_sortable_list = [Zone.city, Zone.district]
+    form_columns = [Zone.city, Zone.district]
 
 
 class ApartmentAdmin(ModelView, model=Apartment):
     column_list = [
         Apartment.id, Apartment.name, Apartment.project, Apartment.address,
         Apartment.section, Apartment.floor, Apartment.area,
-        Apartment.image, Apartment.price, Apartment.discounted_price
+        Apartment.image, Apartment.price, Apartment.discounted_price,
+        Apartment.zone
     ]
     column_searchable_list = [
         Apartment.name, Apartment.project, Apartment.address
@@ -18,5 +26,13 @@ class ApartmentAdmin(ModelView, model=Apartment):
     form_columns = [
         Apartment.name, Apartment.project, Apartment.address,
         Apartment.section, Apartment.floor, Apartment.area,
-        Apartment.price, Apartment.discounted_price, Apartment.image
+        Apartment.price, Apartment.discounted_price, Apartment.image,
+        Apartment.zone
     ]
+
+    form_ajax_refs = {
+        'zone': {
+            'fields': ('id', 'city', 'district'),
+            'order_by': 'city',
+        }
+    }
