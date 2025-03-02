3f312c1949b2954305dcabdb99154b027574b073
qdi2k <qdi2k@mail.ru>
2025-03-03 00:32:43 +0300

    Очередной пре request запрос на поиск квартиры

--- backend/app/api/schema/apartment.py
+++ backend/app/api/schema/apartment.py
@@ -1,12 +1,14 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
-from core.enums import CountRooms
+from app.core.enums import CountRooms
 
 
 class RequestSearchApartment(BaseModel):
     zone: str = Field(min_length=3, max_length=100)
 
-    rooms_count: CountRooms | None = Field()
-    area: float | None = Field(min_length=5, max_length=1000)
-    floor: int | None = Field(min_length=1, max_length=150)
-    price: float | None = Field(min_length=500000, max_length=300000000)
+    rooms_count: Optional[CountRooms] = Field(default=None)
+    area: Optional[float] = Field(default=None, ge=5, le=1000)
+    floor: Optional[int] = Field(default=None, ge=1, le=150)
+    price: Optional[float] = Field(default=None, ge=500000, le=300000000)
