90847c3ebb3b8c72742102ec9fcf4cfed24a1e7c
qdi2k <qdi2k@mail.ru>
2025-03-07 01:57:04 +0300

    Добавил новые модели
    - Добавил новую модель Project
    
    - Вынес поля из Apartment в ApartmentDetail связь один к одному

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,17 +1,24 @@
 from sqlalchemy import Integer, String, Float, Enum, ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from fastapi_storages.integrations.sqlalchemy import FileType
-from starlette.requests import Request
 
 from app.core.config import APARTMENTS_IMAGE
 from app.db.database import Base
 from app.core.enums import CountRooms
 
 
-class Zone(Base):
-    __tablename__ = 'zones'
+class Project(Base):
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=True)
+    address: Mapped[str] = mapped_column(String(300), nullable=False)
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    # "one to many" with Apartment
+    apartments: Mapped[list["Apartment"]] = relationship(
+        argument="Apartment", back_populates="project"
+    )
+
+
+class Zone(Base):
     city: Mapped[str] = mapped_column(String(100), nullable=False)
     district: Mapped[str] = mapped_column(String(100), nullable=False)
 
@@ -20,16 +27,35 @@ class Zone(Base):
         argument="Apartment", back_populates="zone"
     )
 
-    def __admin_repr__(self, request: Request):
-        return f'{self.city}, {self.district}'
-
 
 class Apartment(Base):
-    __tablename__ = 'apartments'
-
     name: Mapped[str] = mapped_column(String(200), nullable=False)
-    project: Mapped[str] = mapped_column(String(200), nullable=False)
-    address: Mapped[str] = mapped_column(String(300), nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    discounted_price: Mapped[float] = mapped_column(Float, nullable=False)
+    image: Mapped[str] = mapped_column(
+        FileType(storage=APARTMENTS_IMAGE), nullable=True
+    )
+
+    details: Mapped["ApartmentDetail"] = relationship(
+        back_populates="apartment"
+    )
+
+    zone_id: Mapped[int] = mapped_column(
+        ForeignKey("zone.id"), nullable=False
+    )
+    zone: Mapped["Zone"] = relationship(
+        argument="Zone", back_populates="apartment"
+    )
+
+    project_id: Mapped[int] = mapped_column(
+        ForeignKey("project.id"), nullable=False
+    )
+    projects: Mapped["Project"] = relationship(
+        argument="Project", back_populates="apartment"
+    )
+
+
+class ApartmentDetail(Base):
     rooms_count: Mapped[CountRooms] = mapped_column(
         Enum(
             CountRooms,
@@ -39,16 +65,9 @@ class Apartment(Base):
     section: Mapped[str] = mapped_column(String(50), nullable=False)
     floor: Mapped[int] = mapped_column(Integer, nullable=False)
     area: Mapped[float] = mapped_column(Float, nullable=False)
-    price: Mapped[float] = mapped_column(Float, nullable=False)
-    discounted_price: Mapped[float] = mapped_column(Float, nullable=False)
 
-    zone_id: Mapped[int] = mapped_column(
-        ForeignKey("zones.id"), nullable=False
+    # "one-to-one" with Apartment
+    apartment_id: Mapped[int] = mapped_column(
+        ForeignKey("apartment.id"), nullable=False, unique=True
     )
-    zone: Mapped["Zone"] = relationship(
-        argument="Zone", back_populates="apartments"
-    )
-
-    image: Mapped[str] = mapped_column(
-        FileType(storage=APARTMENTS_IMAGE), nullable=True
-    )
+    apartment: Mapped["Apartment"] = relationship(back_populates="apartment_detail")
