a0e0cfa9f4eaedad28b21ffc13a89b1cd3753a1c
qdi2k <qdi2k@mail.ru>
2025-03-09 18:27:34 +0300

    Измененные модели

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy import Integer, String, Float, Enum, ForeignKey
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 from fastapi_storages.integrations.sqlalchemy import FileType
@@ -7,28 +9,9 @@ from app.db.database import Base
 from app.core.enums import CountRooms
 
 
-class Project(Base):
-    name: Mapped[str] = mapped_column(String(200), nullable=False)
-    description: Mapped[str] = mapped_column(String(500), nullable=True)
-    address: Mapped[str] = mapped_column(String(300), nullable=False)
-
-    # "one to many" with Apartment
-    apartments: Mapped[list["Apartment"]] = relationship(
-        argument="Apartment", back_populates="project"
-    )
-
-
-class Zone(Base):
-    city: Mapped[str] = mapped_column(String(100), nullable=False)
-    district: Mapped[str] = mapped_column(String(100), nullable=False)
-
-    # "one to many" with Apartment
-    apartments: Mapped[list["Apartment"]] = relationship(
-        argument="Apartment", back_populates="zone"
-    )
-
-
 class Apartment(Base):
+    """Модель квартир."""
+
     name: Mapped[str] = mapped_column(String(200), nullable=False)
     price: Mapped[float] = mapped_column(Float, nullable=False)
     discounted_price: Mapped[float] = mapped_column(Float, nullable=False)
@@ -37,25 +20,29 @@ class Apartment(Base):
     )
 
     details: Mapped["ApartmentDetail"] = relationship(
-        back_populates="apartment"
+        back_populates="apartments"
     )
 
     zone_id: Mapped[int] = mapped_column(
-        ForeignKey("zone.id"), nullable=False
+        ForeignKey("zones.id"), nullable=False
     )
     zone: Mapped["Zone"] = relationship(
-        argument="Zone", back_populates="apartment"
+        argument="Zone", back_populates="apartments"
     )
 
     project_id: Mapped[int] = mapped_column(
-        ForeignKey("project.id"), nullable=False
+        ForeignKey("projects.id"), nullable=False
     )
-    projects: Mapped["Project"] = relationship(
-        argument="Project", back_populates="apartment"
+    project: Mapped["Project"] = relationship(
+        "Project", back_populates="apartments"
     )
 
 
 class ApartmentDetail(Base):
+    """Модель подробности квартир."""
+
+    __tablename__ = "apartment_details"
+
     rooms_count: Mapped[CountRooms] = mapped_column(
         Enum(
             CountRooms,
@@ -68,6 +55,48 @@ class ApartmentDetail(Base):
 
     # "one-to-one" with Apartment
     apartment_id: Mapped[int] = mapped_column(
-        ForeignKey("apartment.id"), nullable=False, unique=True
+        ForeignKey("apartments.id"), nullable=False, unique=True
     )
-    apartment: Mapped["Apartment"] = relationship(back_populates="apartment_detail")
+    apartments: Mapped["Apartment"] = relationship(back_populates="details")
+
+
+class Project(Base):
+    """Модель проектов."""
+
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=True)
+
+    # "one to many" with Zone
+    zones: Mapped[List["Zone"]] = relationship(
+        argument="Zone", back_populates="project"
+    )
+    # "one to many" with Apartment
+    apartments: Mapped[List["Apartment"]] = relationship(
+        argument="Apartment", back_populates="project"
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Zone(Base):
+    """Модель зоны."""
+
+    city: Mapped[str] = mapped_column(String(100), nullable=False)
+    district: Mapped[str] = mapped_column(String(100), nullable=False)
+    address: Mapped[str] = mapped_column(String(300), nullable=False)
+
+    # "many to one" with Project
+    project_id: Mapped[int] = mapped_column(
+        ForeignKey("projects.id"), nullable=False
+    )
+    project: Mapped["Project"] = relationship(
+        argument="Project", back_populates="zones"
+    )
+    # "one to many" with Apartment
+    apartments: Mapped[list["Apartment"]] = relationship(
+        argument="Apartment", back_populates="zone"
+    )
+
+    def __str__(self):
+        return f'{self.city}, {self.district}, {self.address}'
