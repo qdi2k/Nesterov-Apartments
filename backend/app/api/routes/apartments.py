bdebefd64d6da250d297a093779b0841c7094cd2
qdi2k <qdi2k@mail.ru>
2025-04-12 23:57:15 +0300

    Добавил ручку для поиска квартир.

--- backend/app/api/routes/apartments.py
+++ backend/app/api/routes/apartments.py
@@ -1,31 +1,67 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from app.api.schema.apartment import RequestSearchApartment
-from app.utils.unitofwork import IUnitOfWork, UnitOfWork
-from app.services.apartment import ApartmentService
+from app.api.exeptions import NotFountError
+from app.api.schema.apartment import RequestSearchApartment, ResponseSearchApartment
+from app.db.database import get_async_session
+from app.services.apartment import search_apartments
 
-apartment_router = APIRouter(prefix="/apartment", tags=["apartment"])
-
-
-async def get_apartment_service(
-        uow: IUnitOfWork = Depends(UnitOfWork)
-) -> ApartmentService:
-    """
-    Создает и возвращает экземпляр сервиса аутентификации пользователей.
-    """
-    return ApartmentService(uow)
+apartment_router = APIRouter(prefix="/apartment", tags=["Apartment"])
 
 
 @apartment_router.post(
-    path="/search/",
+    path="/search",
+    response_model=ResponseSearchApartment,
     status_code=status.HTTP_200_OK,
+    responses={**NotFountError().get_error()},
 )
 async def search(
         data: RequestSearchApartment,
-        service: ApartmentService = Depends(get_apartment_service)
-):
+        db: AsyncSession = Depends(get_async_session),
+) -> ResponseSearchApartment:
     """
     ## Поиск квартир
+
+    Получает список квартир, по переданным фильтрам.
+
+    ---
+    #### Принимает на вход следующий параметр:
+    * `city` - название города;
+
+    * `on_sale` - (опциональный) статус в продаже ли квартира;
+
+    * `rooms_count` - (опциональный) список количества комнат квартиры;
+
+    * `min_area` - (опциональный) минимальная площадь квартир;
+
+    * `max_area` - (опциональный) максимальная площадь квартир;
+
+    * `min_floor` - (опциональный) минимальная этаж квартир;
+
+    * `max_floor` - (опциональный) максимальный этаж квартир;
+
+    * `min_price` - (опциональный) минимальная цена квартир;
+
+    * `max_price` - (опциональный) максимальная цена квартир.
+
+    #### Возвращает список квартир состоящих из следующих элементов:
+    * `id` - id квартиры;
+
+    * `name` - название квартиры;
+
+    * `project_name` - название проекта;
+
+    * `construction_date` - дата постройки проекта;
+
+    * `price` - цена квартиры;
+
+    * `discount_percent` - скидка в процентах;
+
+    * `floor` - этаж квартиры;
+
+    * `area` - площадь квартиры.
+
+    * `image` - ссылка на картинку квартиры.
     """
-    return await service.search_apartments(data=data)
+    return await search_apartments(db=db, data=data)
---
+++ backend/app/crud/apartment.py
@@ -0,0 +1,85 @@
+from typing import Sequence, Any, List, Optional
+
+from sqlalchemy import select, and_, Select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api.schema.apartment import RequestSearchApartment
+from app.db.models import Apartment
+
+
+async def get_list_apartments_for_search(
+    db: AsyncSession, data: RequestSearchApartment, project_ids: List[int],
+) -> Sequence[Apartment]:
+    """Поиск квартир по заданным параметрам."""
+    query = select(Apartment).where(
+        Apartment.project_id.in_(project_ids),
+        Apartment.on_sale == data.on_sale
+    )
+    query = _get_filters_for_search(query, data)
+    result = await db.execute(query.order_by(Apartment.price))
+    return result.scalars().all()
+
+
+def _get_filters_for_search(
+        query: Select[Any], data: RequestSearchApartment
+) -> Select[Any]:
+    """Получить фильтры для поиска квартир."""
+    if data.rooms_count:
+        query = query.where(Apartment.rooms_count.in_(data.rooms_count))
+
+    query = _get_filter_by_area(
+        query, min_area=data.min_area, max_area=data.max_area
+    )
+    query = _get_filter_by_floor(
+        query, min_floor=data.min_floor, max_floor=data.max_floor
+    )
+    query = _get_filter_by_price(
+        query, min_price=data.min_price, max_price=data.max_price
+    )
+    return query
+
+
+def _get_filter_by_area(
+        query: Select[Any], min_area: Optional[float], max_area: Optional[float]
+) -> Select[Any]:
+    """Получить фильтр по площади."""
+    area_filters = []
+    if min_area:
+        area_filters.append(Apartment.area >= min_area)
+    if max_area:
+        area_filters.append(Apartment.area <= max_area)
+    if area_filters:
+        query = query.where(and_(*area_filters))
+    return query
+
+
+def _get_filter_by_floor(
+        query: Select[Any], min_floor: Optional[float], max_floor: Optional[float]
+) -> Select[Any]:
+    """Получить фильтр по этажу."""
+    floor_filters = []
+    if min_floor:
+        floor_filters.append(Apartment.floor >= min_floor)
+    if max_floor:
+        floor_filters.append(Apartment.floor <= max_floor)
+    if floor_filters:
+        query = query.where(and_(*floor_filters))
+    return query
+
+
+def _get_filter_by_price(
+        query: Select[Any], min_price: Optional[int], max_price: Optional[int]
+) -> Select[Any]:
+    """Получить фильтр по цене (учитываем скидку если есть)."""
+    price_filters = []
+    if min_price:
+        price_filters.append(
+            Apartment.price * (1 - Apartment.discount_percent / 100) >= min_price
+        )
+    if max_price:
+        price_filters.append(
+            Apartment.price * (1 - Apartment.discount_percent / 100) <= max_price
+        )
+    if price_filters:
+        query = query.where(and_(*price_filters))
+    return query
