f146a53e7bf290af4dfa2507f1049115493326d2
Danil Novgorodov <1ceteam2403@gmail.com>
2025-05-09 14:41:52 +0300

    Выполнено: - Дополнение к архитектуре

---
+++ frontend/src/shared/config/theme/typography.ts
@@ -0,0 +1,19 @@
+import themeStyles from '@/shared/styles/theme.module.css';
+
+export const fontSizes = {
+  small: themeStyles.small,
+  xSmall: themeStyles.xSmall,
+  sMedium: themeStyles.sMedium,
+  xMedium: themeStyles.xMedium,
+  sLarge: themeStyles.sLarge,
+  large: themeStyles.large,
+  xLarge: themeStyles.xLarge,
+}
+
+export const fontWeights = {
+  light: themeStyles.light,
+  regular: themeStyles.regular,
+  medium: themeStyles.medium,
+  semiBold: themeStyles.semiBold,
+  bold: themeStyles.bold,
+}
