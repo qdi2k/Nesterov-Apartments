c6b569b096d60a1060ce5ff88b99d4095def5aa2
Danil Novgorodov <1ceteam2403@gmail.com>
2025-05-09 14:41:52 +0300

    Выполнено: - Дополнение к архитектуре

---
+++ frontend/src/shared/config/theme/colors.ts
@@ -0,0 +1,22 @@
+import themeStyles from '@/shared/styles/theme.module.css';
+
+export const fontColors = {
+  white: `${themeStyles.white}`,
+  grey: `${themeStyles.grey}`,
+  greyDark: `${themeStyles.greyDark}`,
+  greyLight: `${themeStyles.greyLight}`,
+  greyLightSecond: `${themeStyles.greyLightSecond}`,
+  orange: `${themeStyles.orange}`,
+  blueLight: `${themeStyles.blueLight}`,
+  black: `${themeStyles.black}`,
+}
+
+export const iconColors = {
+  white: 'rgba(255, 255, 255, 1)',
+  grey: 'rgba(162, 162, 162, 1)',
+  greyDark: 'rgba(39, 46, 68, 1)',
+  greyLight: 'rgba(238, 241, 246, 1)',
+  greyLightSecond: 'rgba(209, 213, 225, 1)',
+  orange: 'rgba(255, 77, 0, 1)',
+  black: 'rgba(0, 0, 0, 1)',
+}
